6f886ad02771aa4de4fde42fe5eb318f2a3c57c5
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-06-09 10:08:08 -0400

    main layout for demo

--- stu-client/src/components/App.jsx
+++ stu-client/src/components/App.jsx
@@ -1,27 +1,27 @@
-import logo from '../logo.svg';
 import '../css/App.css';
+import { Grid } from '@mui/material';
 import NavBar from './NavBar';
-import Horaire from './Schedule';
+import Schedule from './Schedule';
+import Menu from './Menu';
+import Availabilities from './Availabilities';
+import QuickSwitch from './QuickSwitch';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Horaire />
       </header>
+      <body className="App-body">
+        <Grid container rowSpacing={1} columnSpacing={{ sm: 6, md: 12 }}>
+          <Grid className="App-left" item xs={12} sm={6}>
+            <Schedule />
+          </Grid>
+          <Grid className="App-right" item xs={12} sm={6}>
+            <Menu />
+          </Grid>
+        </Grid>
+      </body>
     </div>
   );
 }
