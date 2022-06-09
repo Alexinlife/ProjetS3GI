7b986a61726217f30550a6330019411cef4cbf7b
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-06-09 10:08:28 -0400

    add Availabilities component

---
+++ stu-client/src/components/Availabilities.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../css/UdS.css';
+import Box from '@mui/material/Box';
+import { Button, Checkbox, FormControlLabel, FormGroup, Typography } from '@mui/material';
+
+class Availabilities extends React.Component {
+    state = {
+        availabilities: [],
+    }
+
+    render() {
+        return (
+            <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background:paper' }}>
+                <Typography className="UdS-title" variant="h6">Changez vos disponibilités :</Typography>
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox sx={{
+                        color: "#4D8406",
+                        '&.Mui-checked': {
+                            color: "#4D8406",
+                        },
+                    }} defaultChecked />} label="9:00" />
+
+                    <FormControlLabel control={<Checkbox sx={{
+                        color: "#4D8406",
+                        '&.Mui-checked': {
+                            color: "#4D8406",
+                        },
+                    }} />} label="11:00" />
+
+                    <FormControlLabel control={<Checkbox sx={{
+                        color: "#4D8406",
+                        '&.Mui-checked': {
+                            color: "#4D8406",
+                        },
+                    }} />} label="13:00" />
+
+                    <Button className="UdS-btn" variant="contained" component="span">
+                        Enregistrer
+                    </Button>
+                </FormGroup>
+            </Box>
+        );
+    }
+}
+
+export default Availabilities;
