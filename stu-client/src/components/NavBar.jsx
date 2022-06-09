b422ef30cb6e6bff8867902a80bb9ebc12b9a92d
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-06-09 10:10:52 -0400

    responsive NavBar component

--- stu-client/src/components/NavBar.jsx
+++ stu-client/src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../css/NavBar.css';
 // Composants Material-UI
-// import { makeStyles } from '@mui/material/styles';
 import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
 // Icônes
 import PersonIcon from '@mui/icons-material/Person';
@@ -17,11 +16,16 @@ export default function NavBar() {
   return (
     <Box className="nav-box" sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className="nav-tool">
           {/* Nom de l'application */}
-          <Typography variant="h6" className="nav-text" sx={{ flexGrow: 1 }}>Système de Tutorat Facile Universitaire</Typography>
+          <Typography className="nav-title" variant="h6" sx={{ flexGrow: 1 }}>
+            <span className="nav-title-full">Système de Tutorat Facile Universitaire</span>
+            <span className="nav-title-md">Système de Tutorat Facile</span>
+            <span className="nav-title-sm">STU</span>
+            </Typography>
           {/* Profil */}
           <IconButton edge="end" className="nav-btn" color="inherit" aria-label="profile">
+            <Typography edge="end" className="nav-text" sx={{ flexGrow: 1 }}>Alex Lajeunesse</Typography>
             <PersonIcon />
           </IconButton>
           {/* Notifications */}
