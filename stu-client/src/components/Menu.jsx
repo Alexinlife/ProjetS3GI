2f8ba82a5da6ba77a08ec0f37e320b7f823aca06
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-07-19 01:21:15 -0400

    edit layout

--- stu-client/src/components/Menu.jsx
+++ stu-client/src/components/Menu.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/UdS.css';
 import '../css/Menu.css';
-import Box from '@mui/material/Box';
-import { Button, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 
 class Menu extends React.Component {
     render() {
         return (
-            <Box className="Menu-box" sx={{ width: '100%', maxWidth: 360, bgcolor: 'background:paper' }}>
+            <Box>
                 <Typography className="UdS-title" variant="h6">Sélectionnez une option :</Typography>
-                <Link className="UdS-link" to={`/availabilities`}>
-                    <Button className="UdS-btn" variant="contained" component="span">
-                        Je veux échanger
-                    </Button>
-                </Link>
-                <Link className="UdS-link" to={`/availabilities`}>
-                    <Button className="UdS-btn" variant="contained" component="span">
-                        Je suis disponible
-                    </Button>
-                </Link>
-                <Link className="UdS-link" to={`/quick-switch`}>
-                    <Button className="UdS-btn" variant="contained" component="span">
-                        Échange rapide
-                    </Button>
-                </Link>
+                <Box className="menu-box" sx={{ width: '100%', maxWidth: 360 }}>
+                    <Link className="UdS-link" to={`/availabilities`}>
+                        <Button className="UdS-btn" variant="contained" component="span">
+                            Je veux échanger
+                        </Button>
+                    </Link>
+                    <Link className="UdS-link" to={`/availabilities`}>
+                        <Button className="UdS-btn" variant="contained" component="span">
+                            Je suis disponible
+                        </Button>
+                    </Link>
+                    <Link className="UdS-link" to={`/quick-switch`}>
+                        <Button className="UdS-btn" variant="contained" component="span">
+                            Échange rapide
+                        </Button>
+                    </Link>
+                </Box>
             </Box>
         );
     }
