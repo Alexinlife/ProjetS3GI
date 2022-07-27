1cb5ad2d45ce48fe65a86fd16e79305a57cf94e0
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-07-27 14:36:58 -0400

    add sessionService

--- stu-client/src/services/sessionService.jsx
+++ stu-client/src/services/sessionService.jsx
@@ -1,3 +1,23 @@
+// import requestService from './requestService';
+
+// import Keycloak from "http://localhost:8180/js/keycloak.js";
+
+/* export function initKeycloak() {
+    const keycloak = new Keycloak({
+        "realm": "usager",
+        "auth-server-url": "http://localhost:8180/",
+        "ssl-required": "external",
+        "clientId": "frontend",
+        "public-client": true,
+        "confidential-port": 0
+    });
+    keycloak.init({onLoad: 'login-required'}).then(function (authenticated) {
+        alert(authenticated ? 'authenticated' : 'not authenticated');
+
+    }).catch(function () {
+        alert('failed to initialize');
+    });
+} */
 
 export async function authentificate(cip, password) {
     return {
@@ -6,9 +26,10 @@ export async function authentificate(cip, password) {
             exp_token: "aa",
         }
     }
+    // return requestService.post("http://localhost:8089/", { cip: cip, password: password });
 }
 
 export async function logout() {
     localStorage.removeItem("session_token");
     window.location = "/";
-  }
+}
