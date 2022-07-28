47d299052f4d50f116ad24a62d6b1f8f0203a192
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-07-27 21:15:38 -0400

    edit App.jsx

--- stu-client/src/components/App.jsx
+++ stu-client/src/components/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../css/App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import jwtDecode from "jwt-decode";
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+// import jwtDecode from "jwt-decode";
 import { Box, Grid, Typography } from '@mui/material';
+// import * as sessionService from '../services/sessionService';
 import NavBar from './NavBar';
-import Login from './Login';
+import Login from './forms/Login';
 import Schedule from './Schedule';
 import Menu from './Menu';
-import Availabilities from './Availabilities';
-import QuickSwitch from './QuickSwitch';
+import Timetable from './forms/Timetable';
+import QuickSwitch from './forms/QuickSwitch';
 
 export default class App extends React.Component {
   state = {
@@ -16,18 +19,18 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    try {
-      const encodedSessionToken = localStorage.getItem("session_token");
-      console.log(encodedSessionToken);
+    /* try {
       this.setState({
-        sessionToken: jwtDecode(encodedSessionToken)
+        sessionToken: jwtDecode(localStorage.getItem("session_token")),
+        cip: localStorage.getItem("cip"),
       });
     } catch (e) {
       console.error(e);
-    }
-    
+    } */
+
     this.setState({
-      sessionToken: localStorage.getItem("session_token")
+      // sessionToken: localStorage.getItem("session_token"),
+      cip: localStorage.getItem("cip"),
     });
   }
 
@@ -38,7 +41,7 @@ export default class App extends React.Component {
           <NavBar />
         </header>
         <body className="App-body">
-          {this.state.sessionToken ?
+          {/* this.state.sessionToken */ this.state.cip ?
             <Grid container rowSpacing={1} columnSpacing={{ sm: 6, md: 12 }}>
               <Grid className="App-left" item xs={12} sm={6}>
                 <Schedule />
@@ -46,10 +49,13 @@ export default class App extends React.Component {
               <Grid className="App-right" item xs={12} sm={6}>
                 <BrowserRouter>
                   <Routes>
+                    <Route path='*' element={<Navigate to="/" replace />} />
                     {/* Menu principal */}
                     <Route index element={<Menu />} path="/" />
+                    {/* Demande d'échange */}
+                    <Route path="/request-change" element={<Timetable />} />
                     {/* Disponibilités */}
-                    <Route path="/availabilities" element={<Availabilities />} />
+                    <Route path="/availabilities" element={<Timetable />} />
                     {/* Échange rapide */}
                     <Route path="/quick-switch" element={<QuickSwitch />} />
                   </Routes>
@@ -65,6 +71,17 @@ export default class App extends React.Component {
                 </Box>
               </Grid>
             </Grid>}
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
         </body>
       </div>
     );
