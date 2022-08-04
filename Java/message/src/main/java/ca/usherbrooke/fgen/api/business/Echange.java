68db8dfd6a0bda717791cdc7f5aa7a289e80b966
PierreLucMarcotte <marp0501@usherbrooke.ca>
2022-08-04 19:50:00 -0400

    jpeux tu mourir?

--- Java/message/src/main/java/ca/usherbrooke/fgen/api/business/Echange.java
+++ Java/message/src/main/java/ca/usherbrooke/fgen/api/business/Echange.java
@@ -3,10 +3,10 @@ package ca.usherbrooke.fgen.api.business;
 public class Echange {
     public boolean valid;
 
+    public int IdTutorat2;
+    public String App;
+    public String Session;
+
+
     public String cip1;
-    public String cip2;
-    public String app;
-    public String session;
-    public int idtutorat1;
-    public int idtutorat2;
 }
