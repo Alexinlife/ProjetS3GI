f68cb3b5d6c672fb2ccf4b0aca3d51b0d79b5091
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-08-05 11:44:22 -0400

    edit notification template

--- stu-client/src/components/Notifications.jsx
+++ stu-client/src/components/Notifications.jsx
@@ -2,35 +2,42 @@ import React from 'react';
 import '../css/UdS.css';
 import '../css/Notifications.css';
 import { Box, Button, List, ListItem, ListItemText } from '@mui/material/';
+import { toast } from 'react-toastify';
 
 export default class Schedule extends React.Component {
   state = {
     notifications: [],
   }
 
+  componentDidMount() {
+    this.getNotifications();
+  }
+
+  async getNotifications() {
+
+  }
+
+  acceptNotification = () => {
+    toast.success("Demande acceptée");
+  };
+
+  declineNotification = () => {
+    toast.success("Demande refusée");
+  };
+
   render() {
     return (
       <Box className="ntf-notifications">
         <List>
-          <ListItem className="UdS-item">
-            <ListItemText primary={"Tutorat 1"} secondary={"s6eapp2 | 07/06/22 9:00"} />
-            <Button className="ntf-accept" variant="text">Accepter</Button>
-            <Button variant="text">Refuser</Button>
+          <ListItem className="UdS-item" id="0">
+            <ListItemText primary={"Félix veut échanger"} secondary={"07/06/22 9:00 -> 07/06/22 13:00"} />
+            <Button className="ntf-accept" onClick={this.acceptNotification} variant="text">Accepter</Button>
+            <Button variant="text" onClick={this.declineNotification}>Refuser</Button>
           </ListItem>
-          <ListItem className="UdS-item">
-            <ListItemText primary={"Tutorat 1"} secondary={"s6eapp2 | 07/06/22 9:00"} />
-            <Button variant="text">Accepter</Button>
-            <Button variant="text">Refuser</Button>
-          </ListItem>
-          <ListItem className="UdS-item">
-            <ListItemText primary={"Tutorat 1"} secondary={"s6eapp2 | 07/06/22 9:00"} />
-            <Button variant="text">Accepter</Button>
-            <Button variant="text">Refuser</Button>
-          </ListItem>
-          <ListItem className="UdS-item">
-            <ListItemText primary={"Tutorat 1"} secondary={"s6eapp2 | 07/06/22 9:00"} />
-            <Button variant="text">Accepter</Button>
-            <Button variant="text">Refuser</Button>
+          <ListItem className="UdS-item" id="0">
+            <ListItemText primary={"Félix veut échanger"} secondary={"07/06/22 9:00 -> 07/06/22 13:00"} />
+            <Button className="ntf-accept" onClick={this.acceptNotification} variant="text">Accepter</Button>
+            <Button variant="text" onClick={this.declineNotification}>Refuser</Button>
           </ListItem>
         </List>
       </Box>
